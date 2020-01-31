d547380dbea1e35932834891c343496fa10a645d
YoonseoPark <qkrdbstj03@naver.com>
2020-01-31 18:48:08 +0900

    made select_file component

---
+++ src/components/Select_File.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <v-select
+                v-model="model"
+                :items="items"
+                label="select your file"
+                outlined
+        ></v-select>
+    </div>
+</template>
+
+<script>
+    export default{
+        data: () => ({
+            items: ['a', 'b', 'c'],
+            model: null
+        }),
+    }
+</script>
